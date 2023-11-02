0092f06e3a5e47c6a56b2487cb264a8d06a3493c
camilo <jcamilomillan18912@gmail.com>
2023-11-02 18:13:04 -0500

    second commit

--- src/PerfilPages.jsx
+++ src/PerfilPages.jsx
@@ -6,10 +6,14 @@ export const PerfilPages = () => {
 
      const handleScroll = () => {
          // Coloca aquí la lógica para manejar el evento de desplazamiento
-         if(window.scrollY === 508){
-              navigate('/homepage/quiensoypage')
+        //  if(window.scrollY === 508){
+        //       navigate('/homepage/quiensoypage')
          
-        }
+        // }
+        if(window.scrollY === 495){
+          navigate('/homepage/quiensoypage')
+     
+    }
        console.log(window.scrollY);
        };
        useEffect(() => {
